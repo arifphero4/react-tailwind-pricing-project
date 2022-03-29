874486741cb12afa8392c3a7309dc07d94eefd23
arifphero <ahammadarif232@gmail.com>
2022-03-29 12:26:59 +0600

    create pricingOption component

--- src/components/Pricing/Pricing.js
+++ src/components/Pricing/Pricing.js
@@ -1,9 +1,20 @@
 import React from "react";
+import PricingOption from "../PricingOption/PricingOption";
 
 const Pricing = () => {
+  const pricingIptions = [
+    { id: 1, name: "Free", price: 0 },
+    { id: 2, name: "Regular", price: 8.99 },
+    { id: 3, name: "Premium", price: 17.89 },
+  ];
   return (
     <div className="bg-indigo-300 p-4 mt-8">
       <h2 className="text-6xl font-mono text-white">Best Deals of the Town!</h2>
+      <div>
+        {pricingIptions.map((option) => (
+          <PricingOption key={option.id} option={option}></PricingOption>
+        ))}
+      </div>
     </div>
   );
 };
---
+++ src/components/PricingOption/PricingOption.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const PricingOption = () => {
+  return <div></div>;
+};
+
+export default PricingOption;
