41466eae70cf534b1d394aa85e62b329093877ae
arifphero <ahammadarif232@gmail.com>
2022-03-29 12:47:29 +0600

    make  benifit fake data

--- src/components/Pricing/Pricing.js
+++ src/components/Pricing/Pricing.js
@@ -3,14 +3,47 @@ import PricingOption from "../PricingOption/PricingOption";
 
 const Pricing = () => {
   const pricingIptions = [
-    { id: 1, name: "Free", price: 0 },
-    { id: 2, name: "Regular", price: 8.99 },
-    { id: 3, name: "Premium", price: 17.89 },
+    {
+      id: 1,
+      name: "Free",
+      price: "0.0",
+      benefits: [
+        "lifetime free",
+        "unlimited deals",
+        "localized deals",
+        "fatastic deals",
+        "crazy deals",
+      ],
+    },
+    {
+      id: 2,
+      name: "Regular",
+      price: 8.99,
+      benefits: [
+        "everything on free ",
+        "unlimited deals",
+        "localized deals",
+        "fatastic deals",
+        "crazy deals",
+      ],
+    },
+    {
+      id: 3,
+      name: "Premium",
+      price: 17.89,
+      benefits: [
+        "everything on regular ",
+        "unlimited deals",
+        "localized deals",
+        "fatastic deals",
+        "crazy deals",
+      ],
+    },
   ];
   return (
     <div className="bg-indigo-300 p-4 mt-8">
       <h2 className="text-6xl font-mono text-white">Best Deals of the Town!</h2>
-      <div>
+      <div className="grid md:grid-cols-3 gap-3 mt-8">
         {pricingIptions.map((option) => (
           <PricingOption key={option.id} option={option}></PricingOption>
         ))}
--- src/components/PricingOption/PricingOption.js
+++ src/components/PricingOption/PricingOption.js
@@ -1,7 +1,23 @@
+import { CheckCircleIcon } from "@heroicons/react/solid";
 import React from "react";
 
-const PricingOption = () => {
-  return <div></div>;
+const PricingOption = (props) => {
+  const { name, price } = props.option;
+  return (
+    <div className="bg-white p-4 rounded-lg">
+      <h2 className="bg-indigo-300 py-2 rounded text-xl font-bold">{name}</h2>
+      <p>
+        <span className="text-5xl">{price}</span>
+        <span className="text-xl text-gray-500">/mo</span>
+      </p>
+      <div>
+        <h3 className="text-xl text-left">Benefits:</h3>
+        <p>
+          <CheckCircleIcon className="w-4 h-4 text-green-500"></CheckCircleIcon>{" "}
+        </p>
+      </div>
+    </div>
+  );
 };
 
 export default PricingOption;
