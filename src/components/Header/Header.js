703b7acc4d3b9e3f55de16d531f3a1c369426bdb
arifphero <ahammadarif232@gmail.com>
2022-03-29 11:27:16 +0600

    first commit

---
+++ src/components/Header/Header.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <div>
+      <h2 className="text-6xl">welcome to tailwind pricing counter!</h2>
+    </div>
+  );
+};
+
+export default Header;
